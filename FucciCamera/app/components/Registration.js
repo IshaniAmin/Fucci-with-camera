215b86952570fc7fc63a838ad6f64540fa177649
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-06-17 23:50:26 -0400

    added test app to fucci

--- FucciCamera/app/components/Registration.js
+++ FucciCamera/app/components/Registration.js
@@ -1,37 +1,130 @@
-import React from 'react'
+'use strict';
 
-import { View, Text, StyleSheet, ListView, TextInput } from 'react-native'
+import React, {Component} from 'react';
+import ReactNative from 'react-native';
+const firebase = require('firebase');
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js')
 
-export default class Main extends React.Component {
+const {
+  AppRegistry,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  AlertIOS,
+} = ReactNative;
 
-  
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDyo8RjI1fSoJKkFKbw3eSftTairyXpEIA",
+    authDomain: "fucci-eddb1.firebaseapp.com",
+    databaseURL: "https://fucci-eddb1.firebaseio.com",
+    storageBucket: ""
+  };
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+
+export default class Registration extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    const rootRef = firebase.database().ref();
+    // const itemsRef = rootRef.child('items');
+
+    return rootRef;
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  // when the component is first rendered, this will be called
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
 
   render() {
     return (
-    <View style={styles.mainContainer}>
-      <Text>Search Team/League</Text>
-      <TextInput
-            style={styles.searchInput}/>
-    </View>
+      <View style={styles.container}>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
+      </View>
+    )
+  }
+
+  _addItem() {
+    AlertIOS.prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
     );
   }
-}
-    
-const styles = StyleSheet.create({
-  mainContainer : {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
 
-  },
-  searchInput: {
-    height: 50,
-    padding: 4,
-    marginRight: 5,
-    fontSize: 23,
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 8,
-    color: 'black'
-    },
-});
+  // The render() function is the main view of the app, and _renderItem() sets the individual items in the list.
+  _renderItem(item) {
+
+    const onPress = () => {
+      AlertIOS.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
+  }
+
+}
